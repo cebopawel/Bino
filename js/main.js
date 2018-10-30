4f89814531e8c5ab0eaaee2d7b69f4424d35ba32
cebopawel <cebo.pawel@gmail.com>
2018-10-30 07:02:58 +0100

    final

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 window.onload = function(){
     document.querySelector(".navbar-toggler-icon").style.setProperty("background-image","url('/img/hamburger.png')")
     ;}
-    
+
 const contmain = document.querySelector(".menu-container");
 
 document.querySelector(".navbar-toggler-icon").addEventListener("click", function(){
     if (!document.querySelector(".navbar-collapse").classList.contains('show')){
         contmain.classList.add("mobile-menu")
-        document.querySelector(".navbar-toggler-icon").style.setProperty("background-image","url('/img/close-icon.png')")
-        
+        document.querySelector(".navbar-toggler-icon").style.setProperty("background-image","url('/img/close-icon.png')") 
     } 
     else {
         contmain.classList.remove("mobile-menu")
         document.querySelector(".navbar-toggler-icon").style.setProperty("background-image","url('/img/hamburger.png')")
-        
     }
 });
 
